4842963af6fe6a3ca3cc96975e956617c2344ed0
d.kashapov <d.kashapov@tinkoff.ru>
2022-04-22 22:57:33 +0300

    feat: Added functionality for choosing the order of output messages from vararg

--- src/main/java/com/tcs/edu/Application.java
+++ src/main/java/com/tcs/edu/Application.java
@@ -3,14 +3,16 @@ package com.tcs.edu;
 import com.tcs.edu.printer.MessageService;
 
 import static com.tcs.edu.decorator.Severity.*;
+import static com.tcs.edu.decorator.MessageOrder.*;
 
 class Application {
     public static void main(String[] args) {
-        MessageService.processMessage(MAJOR, "Hello world");
-        MessageService.processMessage(MAJOR, "Hello world");
-        MessageService.processMessage(MAJOR, "Hello world");
-        MessageService.processMessage(MAJOR, "Hello world");
-        MessageService.processMessage(MINOR, "Hello world");
-        MessageService.processMessage(MAJOR, "Hello world");
+        MessageService.processMessage(
+                MAJOR,
+                DESC,
+                "Hello world 1",
+                "Hello world 2", "Hello world 3", "Hello world 4",
+                          "Hello world 5", "Hello world 6"
+            );
     }
 }
--- src/main/java/com/tcs/edu/printer/MessageService.java
+++ src/main/java/com/tcs/edu/printer/MessageService.java
@@ -1,25 +1,40 @@
 package com.tcs.edu.printer;
 
+import com.tcs.edu.decorator.MessageOrder;
 import com.tcs.edu.decorator.Severity;
 import com.tcs.edu.decorator.SeverityDecorator;
 import com.tcs.edu.decorator.TimestampMessageDecorator;
 
+import static com.tcs.edu.decorator.MessageOrder.*;
+
 public class MessageService {
 
-    public static void processMessage(Severity severity, String message, String... messages) {
+    public static void processMessage(Severity severity, MessageOrder order, String message, String... messages) {
 
-        if (severity != null && message != null) {
+        if (severity == null || message == null) {return;}
+
+        if (order == ASC) {
             ConsolePrinter.print(TimestampMessageDecorator.decorate(SeverityDecorator.decorate(severity,message)));
 
-            for (String messageItem: messages) {
+            for (int i = 0; i < messages.length; i++) {
 
-                if (messageItem != null) {
-                    ConsolePrinter.print(TimestampMessageDecorator.decorate(SeverityDecorator.decorate(severity,messageItem)));
+                if (messages[i] != null) {
+                    ConsolePrinter.print(
+                            TimestampMessageDecorator.decorate(SeverityDecorator.decorate(severity,messages[i]))
+                    );
                 }
             }
+        } else if (order == DESC) {
+            for (int i = messages.length - 1; i >= 0; i--) {
+
+                if (messages[i] != null) {
+                    ConsolePrinter.print(
+                            TimestampMessageDecorator.decorate(SeverityDecorator.decorate(severity,messages[i]))
+                    );
+                }
+            }
+
+            ConsolePrinter.print(TimestampMessageDecorator.decorate(SeverityDecorator.decorate(severity,message)));
         }
-
-
-
     }
 }
